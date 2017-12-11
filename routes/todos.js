c285a071500224a345b8eaaf28cd2982efd575f7
jamesboyer92 <jamesboyer92@user.noreply.github.com>
2017-12-10 20:07:17 -0500

    added show route

--- routes/todos.js
+++ routes/todos.js
@@ -13,7 +13,23 @@ router.get('/', function(req, res){
 });
 
 router.post('/', function(req, res){
-	console.log(req.body);
+	db.Todo.create(req.body)
+	.then(function(newTodo){
+		res.status(201).json(newTodo);
+	})
+	.catch(function(err){
+		res.send(err);
+	})
+});
+
+router.get('/:todoId', function(req, res){
+	db.Todo.findById(req.params.todoId)
+	.then(function(foundTodo){
+		res.json(foundTodo)
+	})
+	.catch(function(err){
+		res.send(err);
+	})
 });
 
 module.exports = router;
