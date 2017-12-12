1d1dc2da3b0646de873f556067248bcdf302a1e6
jamesboyer92 <jamesboyer92@user.noreply.github.com>
2017-12-12 18:26:31 -0500

    added api call with jQuery, created addTodos function to pull todos from api to my index.html, added class to Todos to style.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,14 @@
-alert("hi from app.js!");
+$(document).ready(function(){
+	$.getJSON("/api/todos")
+	.then(addTodos)
+});	
+
+function addTodos(todos){
+	//add todos to page here
+	todos.forEach(function(todo){
+		var newTodo = $('<li>'+todo.name+'</li>');
+		newTodo.addClass('task')
+		$('.list').append(newTodo);
+
+	});
+}
