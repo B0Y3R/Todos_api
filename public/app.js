3f87e6629dd8423fc391e1f09a3f6f3429274bf0
jamesboyer92 <jamesboyer92@user.noreply.github.com>
2017-12-12 19:33:04 -0500

    added todo function, create todo function, and addTodo function, used Jquery to manipulate the DOM in order to update todos to list

--- public/app.js
+++ public/app.js
@@ -1,14 +1,40 @@
 $(document).ready(function(){
 	$.getJSON("/api/todos")
 	.then(addTodos)
+
+
+	$('#todoInput').keypress(function(event){
+		if(event.which == 13) {
+			createTodo();
+		}
+	})
 });	
 
 function addTodos(todos){
 	//add todos to page here
 	todos.forEach(function(todo){
-		var newTodo = $('<li>'+todo.name+'</li>');
-		newTodo.addClass('task')
-		$('.list').append(newTodo);
-
+		addTodo(todo);
 	});
+}
+
+function addTodo(todo){
+	var newTodo = $('<li>'+ todo.name +'</li>');
+		newTodo.addClass('task')
+		
+	if(todo.completed){
+		newTodo.addClass('done');
+	}
+	$('.list').append(newTodo);
+}
+
+function createTodo(){
+	//send request to create a new todo
+	var usrInput = $('#todoInput').val();
+	$.post('/api/todos',{name: usrInput})
+	.then(function(newTodo){
+		addTodo(newTodo);
+	})
+	.catch(function(err){
+		console.log(err);
+	})
 }
